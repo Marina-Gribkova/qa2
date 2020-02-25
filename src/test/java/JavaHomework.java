29b59446315eb4e04b6f958aed8e4c8a1a5a0f89
Marina-Gribkova <marina.gribkova1@gmail.com>
2020-02-25 10:44:17 +0200

    JavaHomeWork

---
+++ src/test/java/JavaHomework.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+public class JavaHomework {
+    @Test
+    public void JavaHomework () {
+      String name = "Дима" ;
+      int age = 33 ;
+        System.out.println("Привет, " + name + ", тебе увы аж " + age);
+
+
+        double k = 250 ;
+        double t = 25 ;
+        System.out.println("Расход " + (t / k * 100)) ;
+
+
+        double r = 10 ;
+
+        System.out.println("Длина окружности = " + 2 * Math.PI * r) ;
+        System.out.println("Сделает " + 1000 / (2 * Math.PI * r) + " оборотов");
+
+
+        }
+    }
