9fe441661982c7ccb0249e2de926f468647e7c63
Marina-Gribkova <marina.gribkova1@gmail.com>
2020-05-06 16:54:47 +0300

    BackendHomeWork

---
+++ src/test/java/model/Sys.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Sys {
+    private long type;
+    private long id;
+    private float message;
+    private String country;
+    private float sunrise;
+    private float sunset;
+
+    public long getType() {
+        return type;
+    }
+
+    public void setType(long type) {
+        this.type = type;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public float getMessage() {
+        return message;
+    }
+
+    public void setMessage(float message) {
+        this.message = message;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public float getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(float sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public float getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(float sunset) {
+        this.sunset = sunset;
+    }
+}
