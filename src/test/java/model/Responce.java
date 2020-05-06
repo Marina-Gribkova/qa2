7333d581401fc34b75a09aa19d9f146eae93e8fa
Marina-Gribkova <marina.gribkova1@gmail.com>
2020-05-06 16:53:00 +0300

    BackendHomeWork

---
+++ src/test/java/model/Responce.java
@@ -0,0 +1,121 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Responce {
+    private Coord coord;
+    private String base;
+    private Float visibility;
+    private Float dt;
+    private Long id;
+    private String name;
+    private Long cod;
+    private Main main;
+    private Wind wind;
+    private Clouds clouds;
+    private Sys sys;
+
+    @JsonProperty("weather")
+    private List<Weather> weathers;
+
+    public Coord getCoord() {
+        return coord;
+    }
+
+    public void setCoord(Coord coord) {
+        this.coord = coord;
+    }
+
+    public List<Weather> getWeathers() {
+        return weathers;
+    }
+
+    public void setWeathers(List<Weather> weathers) {
+        this.weathers = weathers;
+    }
+
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public Float getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(Float visibility) {
+        this.visibility = visibility;
+    }
+
+    public Float getDt() {
+        return dt;
+    }
+
+    public void setDt(Float dt) {
+        this.dt = dt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCod() {
+        return cod;
+    }
+
+    public void setCod(Long cod) {
+        this.cod = cod;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public void setWind(Wind wind) {
+        this.wind = wind;
+    }
+
+    public Clouds getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(Clouds clouds) {
+        this.clouds = clouds;
+    }
+
+    public Sys getSys() {
+        return sys;
+    }
+
+    public void setSys(Sys sys) {
+        this.sys = sys;
+    }
+}
